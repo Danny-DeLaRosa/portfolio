4969c7c90971f73ba371e1b2cffad91e4ac362aa
Danny De La Rosa <danny.devlarosa@gmail.com>
2024-02-16 15:19:34 -0800

    About page text

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,7 +14,7 @@ const About = () => {
         <h1>About Danny</h1>
       </div>
       <div className="about-text">
-        <p>I'm Danny, a full-stack junior software engineer with an emphasis on backend development.<br/> <br/> I enjoy collaborating on fun projects; if you are interested in working together, please feel free to reach out! I hail from San Diego, CA, and have a special love for beer, coffee, and food! Let's grab a drink if you're in my neighborhood! </p>
+        <p>I'm Danny, a full-stack junior software engineer with an emphasis on backend development.<br/> <br/> I enjoy collaborating on fun projects; if you are interested in working together, please feel free to reach out!<br /><br /> I hail from San Diego, CA, and have a special love for beer, coffee, and food! Let's grab a drink if you're in my neighborhood! </p>
       </div>
     </main>
   );
